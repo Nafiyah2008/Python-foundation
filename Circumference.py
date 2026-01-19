98d85edb11a8a753958151a5738d06f5e6d79aad
Nafiyah2008 <nafiyahsumbal@gmail.com>
2026-01-19 19:53:22 +0500

    Circumference

--- Circumference.py
+++ Circumference.py
@@ -1,2 +1,4 @@
-import turtle 
-turtle.Screen().bgcolor("Yellow")
+n = int(input("Enter the readius of circle in cm : "))
+formula = 2*3.14
+answer = formula * n
+print("The circumference of the circle is", answer,"cm")
